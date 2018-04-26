0674f2b540f68832339a262211b87aafd35b0fea
DaoMengLi <2322754493@qq.com>
2018-04-27 00:07:47 +0800

    单例模式

---
+++ Design_Pattern/Singleton.cpp
@@ -0,0 +1,83 @@
+
+//
+// Created by lidaomeng on 2018/4/26.
+//
+
+#include <mutex> // 使用C++11互斥锁
+#include <iostream>
+#include <string>
+using namespace std;
+
+std::mutex m_Mutex;
+
+class Person{
+private:
+    string m_Name;
+    unsigned m_Age;
+public:
+    Person():m_Age(0){}
+    Person(const string& aoName, unsigned aiAge):m_Name(aoName), m_Age(aiAge){}
+
+public:
+    void SetPerson(const string& aoName, unsigned aiAge){
+        m_Name = aoName;
+        m_Age = aiAge;
+    }
+    void Print_Person(){
+        cout << "Name: " << m_Name << ", Age: " << m_Age << endl;
+    }
+};
+
+// 版本1 类模板不带参数
+template <class T>
+class Singleton{
+protected:
+    Singleton(){}
+    ~Singleton(){}
+private:
+    Singleton(const Singleton&);
+    Singleton& operator=(const Singleton&);
+
+    //static std::mutex m_Mutex;
+    static T* m_pInstance;
+public:
+    static T* GetInstance();
+    static void DeleteInstance();
+};
+
+template <class T>
+T* Singleton<T>::m_pInstance = nullptr;
+
+template <class T>
+T* Singleton<T>::GetInstance() {
+    if(m_pInstance == nullptr){
+        m_Mutex.lock();
+        if(m_pInstance == nullptr){ // Note: 双判断
+            T* pTemp = new T(); // Note: 临时指针
+            m_pInstance = pTemp;
+        }
+        m_Mutex.unlock();
+    }
+    return m_pInstance;
+}
+
+template <class T>
+void Singleton<T>::DeleteInstance() {
+    if(m_pInstance != nullptr){
+        m_Mutex.lock();
+        if(m_pInstance != nullptr){ // Note: 再次判断指针是否为空，否则会出现释放空指针现象
+            delete m_pInstance;
+            m_pInstance = nullptr;
+        }
+        m_Mutex.unlock();
+    }
+}
+
+int main(){
+    Singleton<Person>::GetInstance()->Print_Person();
+
+    Singleton<Person>::GetInstance()->SetPerson("li dao meng", 99);
+    Singleton<Person>::GetInstance()->Print_Person();
+
+    return 0;
+}
