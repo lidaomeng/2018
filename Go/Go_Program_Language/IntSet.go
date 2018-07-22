f2ef3aea24440f828a8b7892fbd02ceb30f82f4f
DaoMengLi <2322754493@qq.com>
2018-07-22 14:21:16 +0800

    Create IntSet.go

---
+++ Go/Go_Program_Language/IntSet.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+const Plate = 32 << (^uint(0) >> 63)
+
+type IntSet struct {
+	words []uint
+}
+
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/Plate, uint(x%Plate)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+func (s *IntSet) Add(x int) {
+	word, bit := x/Plate, uint(x%Plate)
+
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tWord := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tWord
+		} else {
+			s.words = append(s.words, tWord)
+		}
+	}
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+
+	buf.WriteByte('{')
+
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+
+		for j := 0; j < Plate; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", Plate*i+j)
+			}
+		}
+	}
+
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+func main() {
+	var x, y IntSet
+	x.Add(1)
+	x.Add(9)
+	x.Add(144)
+	fmt.Println(x.String())
+
+	y.Add(9)
+	y.Add(23)
+	fmt.Println(y.String())
+
+	x.UnionWith(&y)
+	fmt.Println(x.String())
+
+	fmt.Println(x.Has(1), x.Has(2))
+}
