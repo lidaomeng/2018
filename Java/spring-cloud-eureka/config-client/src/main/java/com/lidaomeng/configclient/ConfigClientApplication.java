c4186284f87d7c9012dd76402580ed122638ee67
DaoMengLi <2322754493@qq.com>
2019-04-26 18:08:07 +0800

    Add files via upload

---
+++ Java/spring-cloud-eureka/config-client/src/main/java/com/lidaomeng/configclient/ConfigClientApplication.java
@@ -0,0 +1,34 @@
+package com.lidaomeng.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 配置中心客户端
+ *
+ * @author lidaomeng
+ */
+@EnableEurekaClient
+@RestController
+@SpringBootApplication
+//@RefreshScope
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+
+    @GetMapping("/foo")
+    public String hi() {
+        return foo;
+    }
+
+}
